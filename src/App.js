5a0fd6607bd997037e007e6f5500db77550f3df2
Slava37 <grante199213@gmail.com>
2018-11-21 11:49:08 +0300

    eject

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Layout from './hoc/Layout/Layout.js'
 
 class App extends React.Component {
     render() {
         return (
-            <div className="App">
-                <h1>Presentation</h1>
-            </div>
+           <Layout>
+
+
+              <div style={{width: 400, border: '1px solid #ccc'}}>
+                  <h1>Layout works</h1>
+              </div>
+           </Layout>
         );
     }
 }
