24b382086a60e13bb7dcee4161a61f6c5d9781b3
Slava37 <grante199213@gmail.com>
2018-12-10 13:51:38 +0300

    actions added

---
+++ src/store/actions/quiz.js
@@ -0,0 +1,48 @@
+import axios from "../../axios/axios-quiz";
+import {FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS, FETCH_QUIZES_ERROR} from "./actionTypes";
+
+export function fetchQuizes() {
+    return async dispatch => {
+        dispatch(fetchQuizesStart())
+
+        try {
+            const response = await axios.get('/quises.json');
+
+            const quizes = []
+
+            Object.keys(response.data).forEach((key, index) => {
+                quizes.push({
+                    id: key,
+                    name: `Тест № ${index + 1}`
+                })
+            });
+
+            dispatch(fetchQuizesSuccess(quizes))
+
+            this.setState({
+                quizes,
+                loading: false
+            })
+        } catch (e) {
+            dispatch(fetchQuizesError(e))
+        }
+    }
+}
+
+export function fetchQuizesStart() {
+    return {
+        type: FETCH_QUIZES_START
+    }
+}
+
+export function fetchQuizesSuccess(quizes) {
+    return {
+        type: FETCH_QUIZES_SUCCESS
+    }
+}
+
+export function fetchQuizesError(e) {
+    return {
+        type: FETCH_QUIZES_ERROR
+    }
+}
